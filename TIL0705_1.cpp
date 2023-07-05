22181551a55d2f859c24584540a6d340d5a10861
hawan105 <hawn105@ajou.ac.kr>
2023-07-05 20:51:49 +0900

    Create TIL0705_1.cpp

---
+++ TIL0705_1.cpp
@@ -0,0 +1,34 @@
+﻿#include <queue>
+#include <iostream>
+using namespace std;
+queue<int> q;
+void const print() {
+	while ( !q.empty()) {
+		cout << q.front() << " ";
+		q.pop();
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	cout << n << " " << k << endl;
+
+	for (int i = 0; i < n; i++) {
+		q.push(i+1);
+	}
+	
+	for (int j = 0; j < n - 1; j++) {
+		for (int l = 0; l < k - 1; l++) {
+			int temp = q.front();
+			q.pop();
+			q.push(temp);
+		}
+		q.pop();
+	}
+
+	print();
+}
+
